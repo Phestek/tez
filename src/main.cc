cb1f47548b72cf9f6cd976f2ec721aafbd95fa0e
unpause <roidl.sebastian@gmail.com>
2017-05-27 21:30:15 +0200

    Removed some commented out code

--- src/main.cc
+++ src/main.cc
@@ -71,35 +71,7 @@ int main(int argc, char* argv[]) {
     std::cout << "Ouput file: " << output_file << "\n";
 
     compile(args, output_file);
-
-    /*if(argc < 2) {
-        print_usage();
-    } else {
-        std::vector<std::string> input_files;
-        std::string output_file = "output.c";
-        std::vector<std::string> command_line_arguments{argv + 1, argv + argc};
-        try {
-            for(std::size_t i = 0; i < command_line_arguments.size(); ++i) {
-                if(command_line_arguments.at(i) == "-o") {
-                    output_file = command_line_arguments.at(++i);
-                } else {
-                    input_files.push_back(command_line_arguments.at(i));
-                }
-            }
-        } catch(const std::out_of_range& e) {
-            std::cerr << "Unexpected end of input parameters. Exiting.\n";
-            return 1;
-        }
-
-        std::cout << "Input files: ";
-        for(auto& f : input_files) {
-            std::cout << f << ' ';
-        }
-        std::cout << '\n';
-        std::cout << "Output file: " << output_file << '\n';
-        
-        compile(input_files, output_file);
-    }*/
+    
     return 0;
 }
 
